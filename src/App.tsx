014979c671c4f0afd48ff25babe729ce472c1c7c
parking_turkeys <roya1060wien@gmail.com>
2026-08-29 15:40:40 -0400

    i will hack this repo

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,11 @@ export default function App() {
           <img className="sofa" src="/sofa.png" alt="" />
           <img className="cat" src="/cat.png" alt="The cat king" />
         </div>
-      </div>
+       <img className="mats" src="/mats.png" alt="materials" />
+       <img className="money" src="/money.png" alt="dough"/>
+       <img className="pop" src="/population.png" alt="cats"/>
+       <img className="happy" src="/happy.png"/>
+    </div>
     </main>
   )
 }
